17605f6bacf41380ac69c4c6269c81dda0545f2c
ps9319 <pmq0101@naver.com>
2023-03-02 17:46:38 +0900

    [박민규] 백준 - 9733

--- week1/minkyu/9733.py
+++ week1/minkyu/9733.py
@@ -19,6 +19,10 @@ for i in range(7):
     print("{} {} {:.2f}".format(work_list[i], work_value[i], work_value[i]/len(work_bee)))
 print("Total {} 1.00".format(len(work_bee)))
 """
+풀이
+벌의 일 목록들을 딕셔너리에 담아두고 value 를 0으로 초기화해준다.
+벌이 한 일을 리스트에 담아두고 딕셔너리에서 확인하고 있으면 1 을 더해준다.
+
 input = sys.stdin.readline
 
 work_bee = []
@@ -30,5 +34,6 @@ while 1:
         
 입력받을 때 이런 식으로 했는데 왜 안되는지 모르겠다..
 
-리스트로 추가돼서 안됐던 거였다..
+리스트로 추가돼서 안됐던 거였다.. 리스트에 더해줬으면 됐겠다.
+ex) [[1,2,3,4],[5,6,7,8]]
 """
