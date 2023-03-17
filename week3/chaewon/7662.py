7a78485933ab98a11cef06ffd8cb7b6b55b48fb4
chaewon215 <203253@jnu.ac.kr>
2023-03-17 17:40:45 +0900

    [김채원] 백준 - 7662

--- week3/chaewon/7662.py
+++ week3/chaewon/7662.py
@@ -12,28 +12,48 @@ max_heap = []
 
 for _ in range(t):
     k = int(sys.stdin.readline().strip())
+    visited = [0] * k
 
-    for _ in range(k):
+    for i in range(k):
         s, n = sys.stdin.readline().strip().split(' ')
         n = int(n)
 
         if s == 'I':
-            heapq.heappush(min_heap, n)
-            heapq.heappush(max_heap, -n)
+            # 값을 추가할 때, 해당 값의 아이디인 i를 튜플 형태로 함께 추가
+            heapq.heappush(min_heap, (n, i))
+            heapq.heappush(max_heap, (-n, i))
+
+            # 추가한 아이디의 visited = 1로 설정
+            visited[i] = 1
 
         if s == 'D':
-            if n == 1 and max_heap:
-                rm = heapq.heappop(max_heap)
-                min_heap.remove(-rm)
-            if n == -1 and min_heap:
-                rm = heapq.heappop(min_heap)
-                max_heap.remove(-rm)
 
+            # 최댓값을 삭제할 때,
+            if n == 1:
 
-if not min_heap:
-    print('EMPTY')
-else:
-    max_ = heapq.heappop(max_heap) * (-1)
-    min_ = heapq.heappop(min_heap)
+                while max_heap and not visited[max_heap[0][1]]:
+                    heapq.heappop(max_heap)
 
-    print(max_, min_)
+                if max_heap:
+                    visited[max_heap[0][1]] = 0
+                    heapq.heappop(max_heap)
+
+            elif n == -1:
+                while min_heap and not visited[min_heap[0][1]]:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    visited[min_heap[0][1]] = 0
+                    heapq.heappop(min_heap)
+
+    while min_heap and not visited[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+    while max_heap and not visited[max_heap[0][1]]:
+        heapq.heappop(max_heap)
+
+    if not min_heap or not max_heap:
+        print('EMPTY')
+    else:
+        max_ = - max_heap[0][0]
+        min_ = min_heap[0][0]
+
+        print(max_, min_)
