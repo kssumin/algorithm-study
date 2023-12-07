184969ce037487b3d3d54325bc42f6b0f74aed16
Kimjongmin <rlajm1203@naver.com>
2023-12-08 01:00:41 +0900

    [김종민] 백준 - 2002번

--- season5/week3/jongmin/추월.py
+++ season5/week3/jongmin/추월.py
@@ -1,24 +1,32 @@
 #2002번
-from itertools import combinations as comb
 N = int(input())
 
 daegeun = {}
 youngsik = {}
-
+cars = []
 cnt = 0
 
+
 for i in range(N):
-    daegeun[input()] = i+1
+    car = input().rstrip()
+    daegeun[car] = i+1
+    cars.append(car)
+    
 
 for i in range(N):
     youngsik[input()] = i+1
 
-for i in daegeun.keys():
-    # youngsik - daegeun 이 0보다 작다면,
-    # 들어갈 때 보다 나갈 때 더 빨리 나간 것이므로
-    # 추월했다고 볼 수 있다.
-    print(i, youngsik[i] - daegeun[i])
-    if youngsik[i] - daegeun[i] < 0:
-        cnt += 1
+#print(cars)
+for i in range(len(cars)):
+    for j in range(len(cars)):
+        # 상대적인 순서가 처음과 같다면 추월하지 않은 것이다.
+        #print(f"car[{i}] = {cars[i]}, car[{j}]={cars[j]}")
+        if (daegeun[cars[i]] > daegeun[cars[j]] and youngsik[cars[i]] < youngsik[cars[j]]):
+            cnt += 1
+            break
 
 print(cnt)
+# a b c d e
+# e a b c d
+# e b c d a
+# 
