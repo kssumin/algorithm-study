1241b8b43c1a57e2098a23d09bda36cebe43564d
kssumin <201566@jnu.ac.kr>
2024-01-05 18:54:53 +0900

    [김수민] 백준 - 15651

---
+++ season5/week5/sumin/week5-sumim/src/Solution_15651.java
@@ -0,0 +1,47 @@
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Solution_15651 {
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            dfs(depth + 1);
+        }
+    }
+}
